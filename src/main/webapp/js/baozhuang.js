cd026142252a201c3a12f2edec79be511a702c89
teng.wang <teng.wang@corp.elong.com>
2016-05-25 16:17:11 +0800

    add css and js

---
+++ src/main/webapp/js/baozhuang.js
@@ -0,0 +1,10 @@
+/**
+ * Created by teng on 25/5/16.
+ */
+$(document).ready(function(){
+    $("#slider").easySlider({
+        auto: true,
+        continuous: true,
+        numeric: true
+    });
+});
